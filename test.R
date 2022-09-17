19ad60be7dca7daa71d1c92e4bbebf96fa761a7b
Jesus Sanchez <113047026+JesusSanchezM@users.noreply.github.com>
2022-09-17 14:29:21 -0500

    commit to push to GitHub

--- test.R
+++ test.R
@@ -1,3 +1,13 @@
 get_yesterday <- function(){
-  return(Sys.Date()-1)
+  x <- (Sys.Date()-1)
+  return(x)
 }
+
+#git reset --hard "type the commit version"+
+#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
+
+library(usethis)
+?use_github
+
+edit_r_environ()
+use_github(protocol='https', autho_token=Sys.)
